4ab4c685858949af72388fb6ae15ed491bae9e48
teeradet025 <201479094+teeradet025@users.noreply.github.com>
2025-03-29 22:52:43 +0700

    PauseMenu

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+         PausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
