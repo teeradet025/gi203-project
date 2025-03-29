99c7d1127fceee03d5475bf6c0523d04fc1f451d
teeradet025 <201479094+teeradet025@users.noreply.github.com>
2025-03-29 23:24:20 +0700

    Music+

---
+++ Assets/Music/BGmusic.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BGmusic : MonoBehaviour
+{
+   public static BGmusic instance;
+     void Awake()
+    {
+      if(instance != null)
+            Destroy(gameObject);
+      else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+}
